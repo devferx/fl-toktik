4141577c89a1f2d5db17053a8f4c245da7d904bb
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-06-29 00:09:42 -0400

    Add Background gradient

--- lib/presentation/widgets/shared/video_player/fullscreen_player.dart
+++ lib/presentation/widgets/shared/video_player/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/shared/video_player/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -24,7 +25,7 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
 
     controller = VideoPlayerController.asset(widget.videoUrl)
       ..setVolume(0)
-      ..setLooping(false)
+      ..setLooping(true)
       ..play();
   }
 
@@ -61,6 +62,7 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
               child: Stack(
                 children: [
                   VideoPlayer(controller),
+                  VideoBackground(stops: const [0.8, 1.0]),
                   Positioned(
                     bottom: 52,
                     left: 24,
