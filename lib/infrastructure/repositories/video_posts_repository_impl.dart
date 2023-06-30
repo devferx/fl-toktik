522b68ef497793b251886ba69435de34b5049bf6
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-06-30 00:02:12 -0400

    Usando el repositorio y origen de datos

--- lib/infrastructure/repositories/video_posts_repository_impl.dart
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -2,10 +2,10 @@ import 'package:toktik/domain/datasources/video_posts_datasource.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/domain/repositories/video_posts_repository.dart';
 
-class VideoPostsRepository implements VideoPostRepository {
+class VideoPostsRepositoryImpl implements VideoPostRepository {
   final VideoPostsDatasource videosDatasource;
 
-  VideoPostsRepository({required this.videosDatasource});
+  VideoPostsRepositoryImpl({required this.videosDatasource});
 
   @override
   Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
